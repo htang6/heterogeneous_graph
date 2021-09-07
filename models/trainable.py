fbabc1cf5b0190c3b0158b5d286f38bd617d3c61
haocheng <haocheng@haochengs-MacBook-Pro.local>
2021-09-07 15:10:21 +0700

    enable some preparation before start evaluate

--- models/trainable.py
+++ models/trainable.py
@@ -4,6 +4,9 @@ class Trainable(torch.nn.Module):
     def train_step(self, idx, batch):
         pass
 
+    def eval_pre(self):
+        pass
+
     def eval_step(self, idx, batch):
         pass
 
--- trainer.py
+++ trainer.py
@@ -1,6 +1,7 @@
 
 from pprint import pformat
 import torch
+from torch.utils.tensorboard import SummaryWriter
 
 class Trainer():
     def __init__(self, model, conf, device, logger) -> None:
@@ -8,9 +9,11 @@ class Trainer():
         self.conf = conf
         self.device = device
         self.logger = logger
+        self.tb_writer = SummaryWriter('default')
 
     def fit(self, train_dl, eval_dl, lr):
-        self.logger.info('Start training...')
+        self.logger.info('Trainer: start training...')
+        b_sz = train_dl.batch_size
         optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.model.zero_grad()
         self.model.train()
@@ -20,20 +23,26 @@ class Trainer():
                 loss = self.model.train_step(idx, batch)
                 if (idx + 1) % self.conf['print_interval'] == 0:
                     self.logger.info('epoch %d batch %d loss %.3f', epoch, idx, loss.item())
+                    total_idx = epoch*b_sz+idx
+                    self.tb_writer.add_scalar('loss', total_idx, loss.item())
                     metrics = self.eval(eval_dl)
                     self.logger.info('Evaluation result: \n' + pformat(metrics))
+                    for k,v in metrics.items():
+                        self.tb_writer.add_scalar(k, total_idx, v)
                 self.model.zero_grad()
                 loss.backward()
                 optimizer.step()
-        self.logger.info('End training...')
+        self.logger.info('Trainer: end training...')
 
     def eval(self, eval_dl):
         eval_list = []
         self.model.eval()
-        for idx, batch in enumerate(eval_dl):
-            batch = batch.to(self.device)
-            result  = self.model.eval_step(idx, batch)
-            eval_list.append(result)
-        metrics = self.model.eval_sum(eval_list)
+        with torch.no_grad():
+            self.model.eval_pre()
+            for idx, batch in enumerate(eval_dl):
+                batch = batch.to(self.device)
+                result  = self.model.eval_step(idx, batch)
+                eval_list.append(result)
+            metrics = self.model.eval_sum(eval_list)
         self.model.train()
         return metrics
