b3022700d830c6f005dae5352dc8516e8a3d65d9
haocheng <haocheng@haochengs-MacBook-Pro.local>
2021-10-15 15:32:47 +0700

    improve printout

--- trainer.py
+++ trainer.py
@@ -1,37 +1,50 @@
-
+import time
 from pprint import pformat
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
 class Trainer():
-    def __init__(self, model, conf, device, logger) -> None:
+    def __init__(self, model, conf, device, logger, acc_num = 1) -> None:
         self.model = model.to(device)
         self.conf = conf
         self.device = device
         self.logger = logger
         self.tb_writer = SummaryWriter('default')
+        self.acc_num = acc_num
 
     def fit(self, train_dl, eval_dl, lr):
         self.logger.info('Trainer: start training...')
-        b_sz = train_dl.batch_size
         optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
         self.model.zero_grad()
         self.model.train()
-        for epoch in range(self.conf['epoch_num']):
+        epoch_num = self.conf['epoch_num']
+        for epoch in range(epoch_num):
+            print('======== Epoch {:} / {:} ========'.format(epoch + 1, epoch_num))
+            loss_list = []
+            t_start = time.time()
             for idx, batch in enumerate(train_dl):
                 batch = batch.to(self.device)
                 loss = self.model.train_step(idx, batch)
-                if (idx + 1) % self.conf['print_interval'] == 0:
-                    self.logger.info('epoch %d batch %d loss %.3f', epoch, idx, loss.item())
-                    total_idx = epoch*b_sz+idx
-                    self.tb_writer.add_scalar('loss', total_idx, loss.item())
-                    metrics = self.eval(eval_dl)
-                    self.logger.info('Evaluation result: \n' + pformat(metrics))
-                    for k,v in metrics.items():
-                        self.tb_writer.add_scalar(k, total_idx, v)
                 self.model.zero_grad()
                 loss.backward()
-                optimizer.step()
+                # Accumulate gradient
+                if (idx + 1) % self.acc_num == 0:
+                    optimizer.step()
+                loss_list.append(loss.detach())
+
+            train_time = time.time() - t_start
+            avg_loss = torch.mean(torch.stack(loss_list))
+            self.logger.info('[Train] Average loss: %.3f', avg_loss.item())
+            self.logger.info('[Train] Time: %d', train_time)
+
+            t_start = time.time()
+            metrics = self.eval(eval_dl)
+            eval_time = time.time() - t_start
+            self.logger.info('[Evaluation] time: %d', eval_time)
+            self.logger.info('[Evaluation] result: \n' + pformat(metrics))
+            for k,v in metrics.items():
+                self.tb_writer.add_scalar(k, v)
+                
         self.logger.info('Trainer: end training...')
 
     def eval(self, eval_dl):
